28ecbf9a84ebf5d81488e7700c0957046e06418e
Uday S <udayds12mld@gmail.com>
2025-02-22 02:14:19 -0500

    created a setup file for ease of modifying and loading paths

--- DataFlow.py
+++ DataFlow.py
@@ -8,21 +8,14 @@ import FolderOperations.TrainingTestingData as ttd
 import os
 
 
-def start_data_flow(root_dir, vid_dir, frames_dir, scale_down_frames_dir, train_frames_dir, test_frames_dir,
+def start_data_flow(vid_dir, frames_dir, scale_down_frames_dir, train_frames_dir, test_frames_dir,
                     training_dataset_dir, testing_dataset_dir, img_width, img_height, num_channels, mean_std_path, batch_size_percent):
-    if not os.path.exists(vid_dir):
-        os.makedirs(vid_dir)
-        return
-
-    os.makedirs(root_dir, exist_ok=True)
-    os.makedirs(frames_dir, exist_ok=True)
-    os.makedirs(scale_down_frames_dir, exist_ok=True)
-    os.makedirs(train_frames_dir, exist_ok=True)
-    os.makedirs(test_frames_dir, exist_ok=True)
-    os.makedirs(training_dataset_dir, exist_ok=True)
-    os.makedirs(testing_dataset_dir, exist_ok=True)
 
     video_paths = [os.path.join(vid_dir, file_name) for file_name in os.listdir(vid_dir)]
+    if len(video_paths) == 0:
+        print("No video files found")
+        return
+
     for video in video_paths:
         ef.save_video_frames(video, frames_dir)
 
--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-import os
-
 import DataFlow as df
+import setup
 
 IMG_HEIGHT = 240
 IMG_WIDTH = 360
 NUM_CHANNELS = 3
 
-ROOT_DIR = 'Data'
-METADATA_DIR = os.path.join(ROOT_DIR, 'metadata')
-VID_DIR = os.path.join(ROOT_DIR, 'Videos')
-FRAMES_DIR = os.path.join(ROOT_DIR, 'Frames')
-INTERMEDIATE_FRAMES_DIR = os.path.join(ROOT_DIR, 'IntermediateFrames')
-SCALE_DOWN_FRAMES_DIR = os.path.join(ROOT_DIR, 'ScaleDown')
-TRAIN_FRAMES_DIR = os.path.join(ROOT_DIR, 'TrainingFramesDataset')
-TEST_FRAMES_DIR = os.path.join(ROOT_DIR, 'TestingFramesDataset')
-TRAINING_DATASET_DIR = os.path.join(ROOT_DIR, 'TrainingDataset')
-TESTING_DATASET_DIR = os.path.join(ROOT_DIR, 'TestingDataset')
-MEAN_STD_file = os.path.join(METADATA_DIR, 'mean_std.pkl')
-os.makedirs(METADATA_DIR, exist_ok=True)
 MODEL_PATH = ""
 VIDEO_NAME = ""
 
+paths = setup.setup()
 
+root = paths[0]
+metadata = paths[1]
+vid_dir = paths[2]
+frames_dir = paths[3]
+intermediate_frames_dir = paths[4]
+scale_down_frames_dir = paths[5]
+input_train_frames_dir = paths[6]
+output_train_frames_dir = paths[7]
+input_training_dataset = paths[8]
+output_training_dataset = paths[9]
+mean_std_file = paths[10]
 
-df.start_data_flow(ROOT_DIR, VID_DIR, FRAMES_DIR, SCALE_DOWN_FRAMES_DIR, TRAIN_FRAMES_DIR, TEST_FRAMES_DIR,
-                TRAINING_DATASET_DIR, TESTING_DATASET_DIR, IMG_HEIGHT, IMG_WIDTH, NUM_CHANNELS, MEAN_STD_file)
+df.start_data_flow(vid_dir, frames_dir, scale_down_frames_dir, input_train_frames_dir, output_train_frames_dir,
+                   input_training_dataset, output_training_dataset, IMG_HEIGHT, IMG_WIDTH, NUM_CHANNELS, mean_std_file, 0.25)
